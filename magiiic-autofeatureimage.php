7b69031640aa98c17988cb2edda4b38b5c5fff50
Olivier van Helden <magic@samuelvanille.com>
2025-01-11 05:50:37 +0100

    plugin scaffold

---
+++ magiiic-autofeatureimage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Magiiic auto feature image
+ * Plugin URI:      https://github.com/magicoli/magiiic-autofeatureimage
+ * Description:     Use first image of the post as Feature Image, if not set
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com/
+ * Text Domain:     magiiic-autofeatureimage
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Magiiic_Autofeatureimage
+ */
+
+// Your code starts here.
